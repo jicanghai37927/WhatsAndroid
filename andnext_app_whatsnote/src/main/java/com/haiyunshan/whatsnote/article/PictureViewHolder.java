b094f149056881d4bbc4d493455772b4400e2142
seethat <65538861@qq.com>
2018-11-11 22:08:26 +0800

    update load picture size algrorithm

--- andnext_app_whatsnote/src/main/java/com/haiyunshan/whatsnote/article/PictureViewHolder.java
+++ andnext_app_whatsnote/src/main/java/com/haiyunshan/whatsnote/article/PictureViewHolder.java
@@ -1,13 +1,22 @@
 package com.haiyunshan.whatsnote.article;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import club.andnext.helper.ClearAssistMenuHelper;
 import club.andnext.widget.TargetSizeImageView;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.load.resource.bitmap.DownsampleStrategy;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.signature.ObjectKey;
 import com.haiyunshan.article.PictureEntity;
 import com.haiyunshan.whatsnote.R;
@@ -19,13 +28,19 @@ public class PictureViewHolder extends ComposeViewHolder<PictureEntity> implemen
 
     public static final int LAYOUT_RES_ID = R.layout.layout_compose_picture_list_item;
 
+    static final String TAG = PictureViewHolder.class.getSimpleName();
+
     View removeBtn;
 
     TargetSizeImageView pictureView;
     EditText editText;
 
+    PictureListener pictureListener;
+
     public PictureViewHolder(ComposeArticleFragment f, View itemView) {
         super(f, itemView);
+
+        this.pictureListener = new PictureListener();
     }
 
     @Override
@@ -57,7 +72,7 @@ public class PictureViewHolder extends ComposeViewHolder<PictureEntity> implemen
             int maxWidth = this.getMaxWidth();
             int width = item.getWidth();
             int height = item.getHeight();
-            if (width > maxWidth / 3) {
+            if (width > maxWidth / 2) {
                 width = maxWidth;
                 height = width * item.getHeight() / item.getWidth();
             }
@@ -67,9 +82,11 @@ public class PictureViewHolder extends ComposeViewHolder<PictureEntity> implemen
 
         {
             RequestOptions options = createRequestOptions(item);
+
             Glide.with(parent)
                     .load(item.getUri())
                     .apply(options)
+                    .listener(pictureListener)
                     .into(pictureView);
         }
     }
@@ -79,26 +96,55 @@ public class PictureViewHolder extends ComposeViewHolder<PictureEntity> implemen
         entity.setText(editText.getText());
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == removeBtn) {
+            this.remove();
+        }
+    }
+
     RequestOptions createRequestOptions(PictureEntity entity) {
         RequestOptions options = new RequestOptions();
 
-        int maxWidth = this.getMaxWidth();
-        int maxHeight = 3 * this.getMaxHeight();
+        // picture size
+        int width = entity.getWidth();
+        int height = entity.getHeight();
 
-        int width = maxWidth;
-        int height = width * entity.getHeight() / entity.getWidth();
-        if (height > maxHeight) {
-            height = maxHeight;
-            width = height * entity.getWidth() / entity.getHeight();
+        // sample it for 1, 2, 4, 8, ...
+        {
+            int maxWidth = this.getMaxWidth();
+            int maxHeight = this.getMaxHeight();
 
-            width = (width > maxWidth)? maxWidth: width;
+            int widthSampleFactor = getSampleFactor(width, maxWidth);
+            int heightSampleFactor = getSampleFactor(height, 6 * maxHeight);
+
+            int sampleFactor = Math.max(widthSampleFactor, heightSampleFactor);
+            sampleFactor = Math.max(1, Integer.highestOneBit(sampleFactor));
+
+            if (sampleFactor > 1) {
+                width /= sampleFactor;
+                height /= sampleFactor;
+            }
         }
 
-        options.override(width, height);
-        options.downsample(DownsampleStrategy.FIT_CENTER);
-        options.signature(new ObjectKey(entity.getSignature()));
+        {
+            options.dontTransform();
+            options.override(width, height);
+            options.downsample(DownsampleStrategy.FIT_CENTER);
+            options.signature(new ObjectKey(entity.getSignature()));
+        }
 
         return options;
+    }
+
+    int getSampleFactor(int source, int target) {
+        int scale = 1;
+
+        while ((source / scale) > target) {
+            scale *= 2;
+        }
+
+        return scale;
     }
 
     int getMaxWidth() {
@@ -125,10 +171,26 @@ public class PictureViewHolder extends ComposeViewHolder<PictureEntity> implemen
         return width;
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v == removeBtn) {
-            this.remove();
+    /**
+     *
+     */
+    private class PictureListener implements RequestListener<Drawable> {
+
+        @Override
+        public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+            return false;
+        }
+
+        @Override
+        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+            if (resource != null && resource instanceof BitmapDrawable) {
+                Bitmap bitmap = ((BitmapDrawable)resource).getBitmap();
+                if (bitmap != null) {
+                    Log.v(TAG, "bitmap width = " + bitmap.getWidth() + ", height = " + bitmap.getHeight());
+                }
+            }
+
+            return false;
         }
     }
 }
